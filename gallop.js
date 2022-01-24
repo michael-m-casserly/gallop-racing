3d092540fe8ca3b5c27350cdacbc778bfbc5c9ef
michael-m-casserly <56838992+michael-m-casserly@users.noreply.github.com>
2022-01-24 16:08:03 -0500

    Moved Nominations to a Module Include

--- gallop.js
+++ gallop.js
@@ -1,2 +1,13 @@
 // Place any sitewide Javascript in here, it is referenced from the banner-head.html include file
 
+function handleSelectStable(elm)
+    {
+       //window.location = elm.value;
+       window.open('https://docs.google.com/spreadsheets/d/' + elm.value, '_blank');
+    }
+  
+function handleSelectNominations(elm)
+    {
+       //window.location = elm.value;
+       window.open('https://www.equibase.com/profiles/Results.cfm?' + elm.value, '_blank');
+    }
