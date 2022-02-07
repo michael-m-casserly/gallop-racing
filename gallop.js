084cd4e844036cf34011bc85373e813567637bf4
michael-m-casserly <56838992+michael-m-casserly@users.noreply.github.com>
2022-02-07 13:46:00 -0500

    GRCW-202

--- gallop.js
+++ gallop.js
@@ -1,2 +1,19 @@
 // Place any sitewide Javascript in here, it is referenced from the banner-head.html include file
-  
+
+//This code is used to ensure that the accordion scrolls to the top of the collapsed content automatically; This had been an issue in mobile devices.
+
+/* USING JQUERY FOR NOW, MAY REVISIT TO SEE IF THIS CAN BE DONE WITH JUST JAVASCRIPT...
+const accordionItems = document.querySelectorAll('.accordion-collapse')
+const acc = document.getElementById('accordionExample')
+
+accordionItems.forEach((el)=>{
+    el.addEventListener('shown.bs.collapse',(e)=>{
+        var scrollOffset = acc.scrollTop + el.parentNode.offsetTop
+        acc.scroll({
+            top: scrollOffset,
+            left: 0,
+            behavior: 'smooth'
+        })
+    })
+})
+*/
