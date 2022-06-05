3c03c8573b7e648feef8d4fab88a22882b9481cb
michael-m-casserly <56838992+michael-m-casserly@users.noreply.github.com>
2022-06-05 13:25:13 -0400

    GRCW-432; GRCW-430; QA Fix

--- gallop.js
+++ gallop.js
@@ -1,5 +1,24 @@
 // Place any sitewide Javascript in here, it is referenced from the banner-head.html include file
 
+function handleSelectStable(elm) {
+  window.open('https://docs.google.com/spreadsheets/d/' + elm.value, '_blank');
+}
+
+function handleSelectContest(elm) {
+    window.location = elm.value;
+  }
+
+function handleSelectStakesCalendar(elm)
+  {
+     window.open(elm.value, '_blank');
+  }
+
+function handleSelectNominations(elm)
+    {
+       window.open('https://www.equibase.com/profiles/Results.cfm?' + elm.value, '_blank');
+    }
+
+
 //This code is used to ensure that the accordion scrolls to the top of the collapsed content automatically; This had been an issue in mobile devices.
 
 /* USING JQUERY FOR NOW, MAY REVISIT TO SEE IF THIS CAN BE DONE WITH JUST JAVASCRIPT...
